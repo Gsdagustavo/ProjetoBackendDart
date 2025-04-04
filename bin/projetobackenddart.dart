4c543f0a56d890a810d3db05b9a97a1cad968cb5
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-04-04 20:58:02 -0300

    criado o projeto com os arquivos base

---
+++ bin/projetobackenddart.dart
@@ -0,0 +1,5 @@
+import 'package:projetobackenddart/projetobackenddart.dart' as projetobackenddart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projetobackenddart.calculate()}!');
+}
---
+++ test/projetobackenddart_test.dart
@@ -0,0 +1,8 @@
+import 'package:projetobackenddart/projetobackenddart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
